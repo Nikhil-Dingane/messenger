76c59ece14008352c26c0b52b271597809760391
nick <nikhil@krishagni.com>
2020-03-30 17:38:05 +0530

    json response

---
+++ src/main/java/com/nikhil/krishagni/messenger/resources/MessageResource.java
@@ -0,0 +1,34 @@
+package com.nikhil.krishagni.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.nikhil.krishagni.messenger.model.Message;
+import com.nikhil.krishagni.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+	
+	MessageService messageService=new MessageService();
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML,MediaType.APPLICATION_JSON})
+	public List<Message> getMessages()
+	{
+		return messageService.getAllMessages();
+	}
+	
+    @GET
+    @Path("/{messageId}")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Message getMessage(@PathParam("messageId") long messageId)  {
+     // We can how use the profileId value to retrieve the record
+    	
+    	return messageService.getMessage(messageId);
+ }
+}
---
+++ src/main/java/com/nikhil/krishagni/messenger/service/MessageService.java
@@ -0,0 +1,52 @@
+package com.nikhil.krishagni.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.nikhil.krishagni.messenger.database.DatabaseClass;
+import com.nikhil.krishagni.messenger.model.Message;
+
+public class MessageService {
+	
+	
+	
+	public MessageService() {
+		messages.put(1L, new Message(1,"this is the first message","nikhil"));
+		messages.put(2L, new Message(2,"this is the second message","nikhil"));
+	}
+
+	private Map<Long,Message> messages=DatabaseClass.getMessages();
+
+	public List<Message> getAllMessages(){
+		
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message addMessage(Message message)
+	{
+		message.setId(messages.size()+1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message)
+	{
+		if(message.getId()<=0)
+		{
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id)
+	{
+		return messages.remove(id);
+	}
+	
+	public Message getMessage(long id)
+	{
+		return messages.get(id);
+	}
+}

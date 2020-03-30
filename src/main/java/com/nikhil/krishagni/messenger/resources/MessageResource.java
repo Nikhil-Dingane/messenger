ae5c5ec02ac03ad58ceac4fa2484fbdf44f277fc
nick <nikhil@krishagni.com>
2020-03-30 18:14:24 +0530

    added post method

--- src/main/java/com/nikhil/krishagni/messenger/resources/MessageResource.java
+++ src/main/java/com/nikhil/krishagni/messenger/resources/MessageResource.java
@@ -3,7 +3,9 @@ package com.nikhil.krishagni.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -23,6 +25,14 @@ public class MessageResource {
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message)
+	{
+		return messageService.addMessage(message);
+	}
+	
     @GET
     @Path("/{messageId}")
     @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
@@ -30,5 +40,7 @@ public class MessageResource {
      // We can how use the profileId value to retrieve the record
     	
     	return messageService.getMessage(messageId);
- }
+    }
+    
+  
 }
